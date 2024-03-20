534f05b17c3ce7a470cf6e140d8106eb2105d4df
Naveen <naveen.kaushik.21@mountblue.tech>
2024-03-20 11:06:54 +0530

    second commit

--- src/components/authentication/Signin.jsx
+++ src/components/authentication/Signin.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Signin = () => {
   return (
     <div>
-      Signin
+      <h2>Welcome! Naveen</h2>
+      <h1>Sign in to</h1>
+      <form>
+        <label>Employee ID</label>
+        <input type='text' placeholder='Enter your Employee ID' />
+        <br/>
+        <label>Password</label>
+        <input type='text' placeholder='Enter your Password' />
+      </form>
     </div>
   )
 }
